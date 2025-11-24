71ecb0bce1fb3c3737e79c4c0b3dd74c882cc38d
romanow <romanowalex@mail.ru>
2025-11-24 20:55:46 +0300

    Try to implement k8s load balancer

--- echo-client/src/main/kotlin/ru/romanow/echo/client/config/WebClientConfiguration.kt
+++ echo-client/src/main/kotlin/ru/romanow/echo/client/config/WebClientConfiguration.kt
@@ -1,5 +1,6 @@
 package ru.romanow.echo.client.config
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
@@ -8,5 +9,6 @@ import org.springframework.web.reactive.function.client.WebClient
 class WebClientConfiguration {
 
     @Bean
+    @LoadBalanced
     fun webClientBuilder(): WebClient.Builder = WebClient.builder()
 }
